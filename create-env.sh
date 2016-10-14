0c51210a132adae3c2b7582fcade0351052fa34d
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-14 23:37:36 +0000

    changes of all 5 parameters

--- create-env.sh
+++ create-env.sh
@@ -13,8 +13,8 @@ aws elb create-load-balancer --load-balancer-name itmo-544 --listeners Protocol=
  
 #aws elb register-instances-with-load-balancer --load-balancer-name itmo-544 --instances $Instance
 
-<<<<<<< HEAD
-aws autoscaling create-launch-configuration --launch-configuration-name $4 --image-id $1 --security-groups sg-1db27864 --key-name inclass --instance-type t2.micro --user-data file://installenv.sh
+
+aws autoscaling create-launch-configuration --launch-configuration-name $4 --image-id $1 --security-groups $3 --key-name $2 --instance-type t2.micro --user-data file://installenv.sh
 
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name server-rg --launch-configuration-name $4 --availability-zone us-west-2b --load-balancer-name itmo-544 --max-size 5 --min-size 2 --desired-capacity $5
 aws autoscaling attach-load-balancers --load-balancer-names itmo-544 --auto-scaling-group-name server-rg
@@ -22,13 +22,4 @@ Instance=`aws ec2 describe-instances --filters "Name=instance-state-code,Values=
 
 aws ec2 wait instance-running --instance-ids $Instance
 
-=======
-aws autoscaling create-launch-configuration --launch-configuration-name itmo-544-lcn --image-id $1 --security-groups $3 --key-name $2 --instance-type t2.micro --user-data file://installenv.sh
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name server-rg --launch-configuration-name itmo-544-lcn --availability-zone us-west-2b --load-balancer-name itmo-544 --max-size 5 --min-size 2 --desired-capacity 3
-aws autoscaling attach-load-balancers --load-balancer-names itmo-544 --auto-scaling-group-name server-rg
-Instance=`aws ec2 describe-instances --filters "Name=instance-state-code,Values=16" --query 'Reservations[*].Instances[*].InstanceId'`
-
-aws ec2 wait instance-running --instance-ids $Instance
-
->>>>>>> fd305a19aa02a8df7805b3818ec5068d2e10f580
