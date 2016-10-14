873123ac93ab56c3b01fc60bb3e318b1f7d1dc82
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-14 23:49:39 +0000

    conditions check

--- create-env.sh
+++ create-env.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#checking conditions
+if [ "$#" -ne 5 ]; then
+   echo "Missing parameters"
+   echo -n "the paramters should be displayed in this format:"
+   echo -n " 1) AMI ID: ami-06b94666"
+   echo -n "2)KEY-NAME: inclass"
+   echo -n "3)security-group: sg-1db27864 "
+   echo -n "4) launch-configuration: Give any name"
+   echo -n "5) COUNT: Number of instance numeric value, minimum 2 maximum 5"
+   exit 0
+else
+echo "all parameters conditions are matching ..we can go ahead"
+fi
+
+
 
 #aws ec2 run-instances --image-id $1 --key-name $2 --security-group-ids $3 --instance-type t2.micro --user-data file://installenv.sh --count 3 --placement 
 #AvailabilityZone=us-west-2b
@@ -9,6 +24,7 @@
 #echo "Instance id are $Instance"
 
 #aws ec2 wait instance-running --instance-ids $Instance
+echo "creating load balancer"
 aws elb create-load-balancer --load-balancer-name itmo-544 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnets subnet-cf82a1ab --security-groups $3
  
 #aws elb register-instances-with-load-balancer --load-balancer-name itmo-544 --instances $Instance
@@ -21,5 +37,5 @@ aws autoscaling attach-load-balancers --load-balancer-names itmo-544 --auto-scal
 Instance=`aws ec2 describe-instances --filters "Name=instance-state-code,Values=16" --query 'Reservations[*].Instances[*].InstanceId'`
 
 aws ec2 wait instance-running --instance-ids $Instance
-
+echo "all done"
 
